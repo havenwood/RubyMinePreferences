6281ea1ffe3865a0123926df59c6e84e69d53d08
Matt Wilson <mattwilson@squareup.com>
2011-01-05 11:52:50 -0800

    Using rubymine 3.0 now

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for dir in colors keymaps; do
- mkdir -p ~/Library/Preferences/RubyMine20/$dir/
- cp -v $dir/* ~/Library/Preferences/RubyMine20/$dir/
+ mkdir -p ~/Library/Preferences/RubyMine30/$dir/
+ cp -v $dir/* ~/Library/Preferences/RubyMine30/$dir/
 done

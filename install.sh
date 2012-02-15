1f5dc5f9b9ccdbae5f2b4d76fe4978c56ba0f274
Square <github@squareup.com>
2012-02-15 13:20:13 -0800

    Install to RubyMine 4.0 also.

--- install.sh
+++ install.sh
@@ -3,4 +3,7 @@
 for dir in colors keymaps; do
  mkdir -p ~/Library/Preferences/RubyMine32/$dir/
  cp -v $dir/* ~/Library/Preferences/RubyMine32/$dir/
+
+ mkdir -p ~/Library/Preferences/RubyMine40/$dir/
+ cp -v $dir/* ~/Library/Preferences/RubyMine40/$dir/
 done
